a3aad4a559fae6f1c784701f418acd303825fe74
Aapiiz <99938025+Aapiiz@users.noreply.github.com>
2022-11-24 14:31:45 +0700

    Bikin ilustrasi sama soal

--- src/Game/hangman.c
+++ src/Game/hangman.c
@@ -5,14 +5,12 @@
 const int IDX_UNDEF = -1;
 const int KESEMPATAN = 10;
 
-typedef struct 
-{
+typedef struct {
     char TabWord[100];
 } word;
 
 typedef struct {
     word kata[10];
-    word soal[100];
     int panjang;
 } ListSoal;
 
@@ -25,85 +23,180 @@ void CreateSimpenan (Simpenan *S){
     S->panjang = IDX_UNDEF;
 }
 
+int printSoal(int urutan){
+    if (urutan == 1){
+        printf("Di rumah makan Padang, selain pakai sendok kita makan pakai?");
+    } else if (urutan == 2){
+        printf("Seseorang yang memimpin sebuah desa, biasanya dipanggil pak?");
+    } else if (urutan == 3){
+        printf("Senikmat-nikmatnya makan di luar lebih nikmat makan di?");
+    } else if (urutan == 4){
+        printf("Ikan bernapas di air dengan?");
+    } else if (urutan == 5){
+        printf("Yang dibeli seorang cowok untuk pasangannya di saat Valentine biasanya?");
+    } else if (urutan == 6){
+        printf("Seorang barber mencukur rambut pelanggan dengan?");
+    } else if (urutan == 7){
+        printf("Lorenzo, Marquez, Rossi kejar-kejaran, salip menyalip di?");
+    } else if (urutan == 8){
+        printf("Burung bisa terbang karena memiliki?");
+    } else if (urutan == 9){
+        printf("Bendera NKRI warnanya?");
+    } else if (urutan == 10){
+        printf("Galang menyimpan buku di…");
+    }
+}
+
 void variasiSoal(int urutan){
     ListSoal L;
     if (urutan == 1){
         L.kata = "TENAGA";
-        L.soal = "Di rumah makan Padang, selain pakai sendok kita makan pakai?";
         L.panjang = 6;
     } else if (urutan == 2){
         L.kata = "NOLEH";
-        L.soal = "Seseorang yang memimpin sebuah desa, biasanya dipanggil pak?";
         L.panjang = 5;
     } else if (urutan == 3){
         L.kata = "TELAN";
-        L.soal = "Senikmat-nikmatnya makan di luar lebih nikmat makan di?";
         L.panjang = 6;
     } else if (urutan == 4){
         L.kata = "TENANG";
-        L.soal = "Ikan bernapas di air dengan?";
         L.panjang = 6;
     } else if (urutan == 5){
         L.kata = "BAYAR";
-        L.soal = "Yang dibeli seorang cowok untuk pasangannya di saat Valentine biasanya?";
         L.panjang = 5;
     } else if (urutan == 6){
         L.kata = "GANTIAN";
-        L.soal = "Seorang barber mencukur rambut pelanggan dengan?";
         L.panjang = 7;
     } else if (urutan == 7){
         L.kata = "BIARKAN";
-        L.soal = "Lorenzo, Marquez, Rossi kejar-kejaran, salip menyalip di?";
         L.panjang = 7;
     } else if (urutan == 8){
         L.kata = "BAKAT";
-        L.soal = "Burung bisa terbang karena memiliki?";
         L.panjang = 5;
     } else if (urutan == 9){
         L.kata = "CUMADUAAJA";
-        L.soal = "Bendera NKRI warnanya?";
         L.panjang = 10;
     } else if (urutan == 10){
         L.kata = "MAS";
-        L.soal = "Galang menyimpan buku di…";
         L.panjang = 3;
     }
 }
-char TampilanGaris(Simpenan Ssalah){
+
+void TampilanGaris(Simpenan Ssalah){
     if (Ssalah.panjang == 1){
         printf("======================================\n");
         printf("                                      \n");
         printf("                                      \n");
         printf("                                      \n");
         printf("                                      \n");
-        printf("                   ||                 \n");
+        printf("                                      \n");
+        printf("                                      \n");
+        printf("                          ||          \n");
         printf("======================================\n");
     } else if (Ssalah.panjang == 2){
         printf("======================================\n");
         printf("                                      \n");
         printf("                                      \n");
         printf("                                      \n");
-        printf("                   ||                 \n");
-        printf("                   ||                 \n");
+        printf("                                      \n");
+        printf("                                      \n");
+        printf("                          ||          \n");
+        printf("                          ||          \n");
         printf("======================================\n");
     } else if (Ssalah.panjang == 3){
         printf("======================================\n");
         printf("                                      \n");
+        printf("                                      \n");
+        printf("                                      \n");
+        printf("                                      \n");
+        printf("                          ||          \n");
+        printf("                          ||          \n");
+        printf("                          ||          \n");
+        printf("======================================\n");
+    } else if (Ssalah.panjang == 4){
+        printf("======================================\n");
+        printf("                                      \n");
+        printf("                                      \n");
+        printf("                   -------||          \n");
+        printf("                          ||          \n");
+        printf("                          ||          \n");
+        printf("                          ||          \n");
+        printf("                          ||           \n");
+        printf("======================================\n");
+    } else if (Ssalah.panjang == 3){
+        printf("======================================\n");
+        printf("                                      \n");
+        printf("                                      \n");
+        printf("                                      \n");
         printf("               ______                 \n");
-        printf("                   ||                 \n");
-        printf("                   ||                 \n");
+        printf("               O   ||                 \n");
+        printf("               I   ||                 \n");
         printf("                   ||                 \n");
         printf("======================================\n");
     } else if (Ssalah.panjang == 4){
         printf("======================================\n");
         printf("                                      \n");
+        printf("                                      \n");
+        printf("                                      \n");
         printf("               ______                 \n");
         printf("               O   ||                 \n");
-        printf("                   ||                 \n");
+        printf("               I   ||                 \n");
         printf("                   ||                 \n");
         printf("======================================\n");
-    } else if (Ssalah.panjang == 3){
+    } else if (Ssalah.panjang == 5){
         printf("======================================\n");
+        printf("                                      \n");
+        printf("                                      \n");
+        printf("                                      \n");
+        printf("               ______                 \n");
+        printf("               O   ||                 \n");
+        printf("               I   ||                 \n");
+        printf("                   ||                 \n");
+        printf("======================================\n");
+    } else if (Ssalah.panjang == 6){
+        printf("======================================\n");
+        printf("                                      \n");
+        printf("                                      \n");
+        printf("                                      \n");
+        printf("               ______                 \n");
+        printf("               O   ||                 \n");
+        printf("               I   ||                 \n");
+        printf("                   ||                 \n");
+        printf("======================================\n");
+    } else if (Ssalah.panjang == 7){
+        printf("======================================\n");
+        printf("                                      \n");
+        printf("                                      \n");
+        printf("                                      \n");
+        printf("               ______                 \n");
+        printf("               O   ||                 \n");
+        printf("               I   ||                 \n");
+        printf("                   ||                 \n");
+        printf("======================================\n");
+    } else if (Ssalah.panjang == 8){
+        printf("======================================\n");
+        printf("                                      \n");
+        printf("                                      \n");
+        printf("                                      \n");
+        printf("               ______                 \n");
+        printf("               O   ||                 \n");
+        printf("               I   ||                 \n");
+        printf("                   ||                 \n");
+        printf("======================================\n");
+    } else if (Ssalah.panjang == 9){
+        printf("======================================\n");
+        printf("                                      \n");
+        printf("                                      \n");
+        printf("                                      \n");
+        printf("               ______                 \n");
+        printf("               O   ||                 \n");
+        printf("               I   ||                 \n");
+        printf("                   ||                 \n");
+        printf("======================================\n");
+    } else if (Ssalah.panjang == 10){
+        printf("======================================\n");
+        printf("                                      \n");
+        printf("                                      \n");
         printf("                                      \n");
         printf("               ______                 \n");
         printf("               O   ||                 \n");
@@ -112,7 +205,8 @@ char TampilanGaris(Simpenan Ssalah){
         printf("======================================\n");
     }
 }
-void TampilanGame(){
+
+void TampilanGame(Simpenan Ssalah, Simpenan *Sbenar){
     printf("Tebakan sebelumnya : ");
 
     printf("Kata : ");
@@ -121,6 +215,7 @@ void TampilanGame(){
 
     printf("Masukan tebakan : ");
 
+    TampilanGaris(Ssalah);
 }
 
 boolean IsFinished (Simpenan Sbenar, Simpenan Ssalah, ListSoal Soal){
