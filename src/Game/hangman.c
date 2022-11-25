2cbc81c17b63e02063fdc0c58ed3c89a2f8ee822
Aapiiz <99938025+Aapiiz@users.noreply.github.com>
2022-11-25 16:30:22 +0700

    rapihin pointer

--- src/Game/hangman.c
+++ src/Game/hangman.c
@@ -1,20 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 #include "../boolean.h"
 
-const char MARK = '_';
-const int KESEMPATAN = 10;
+#define IDX_UNDEF -1
+#define MARK '_'
+#define KESEMPATAN 10
 
 typedef struct {
-    char* kata[KESEMPATAN];
+    char kata[KESEMPATAN];
     int panjang;
 } List;
 
-void CreateList1 (List *LSoal, int digit){
+void CreateList(List *LSoal, int digit){
     int i;
     for(i=0;i<digit;i++){
         LSoal->kata[i] = MARK;
     }
+}
+
+void CreateEmpty(List *Lsalah){
+    (*Lsalah).panjang = IDX_UNDEF;
 }
 
 int printSoal(int urutan){
@@ -44,82 +50,82 @@ int printSoal(int urutan){
 void variasiSoal(int urutan, List L){
     if (urutan == 1){
         L.panjang = 6;
-        L.kata[0] = "T";
-        L.kata[1] = "E";
-        L.kata[2] = "N";
-        L.kata[3] = "A";
-        L.kata[4] = "G";
-        L.kata[5] = "A";
+        L.kata[0] = 'T';
+        L.kata[1] = 'E';
+        L.kata[2] = 'N';
+        L.kata[3] = 'A';
+        L.kata[4] = 'G';
+        L.kata[5] = 'A';
     } else if (urutan == 2){
-        L.kata[0] = "N";
-        L.kata[1] = "O";
-        L.kata[2] = "L";
-        L.kata[3] = "E";
-        L.kata[4] = "H";
+        L.kata[0] = 'N';
+        L.kata[1] = 'O';
+        L.kata[2] = 'L';
+        L.kata[3] = 'E';
+        L.kata[4] = 'H';
         L.panjang = 5;
     } else if (urutan == 3){
-        L.kata[0] = "T";
-        L.kata[1] = "E";
-        L.kata[2] = "L";
-        L.kata[3] = "A";
-        L.kata[4] = "N";
+        L.kata[0] = 'T';
+        L.kata[1] = 'E';
+        L.kata[2] = 'L';
+        L.kata[3] = 'A';
+        L.kata[4] = 'N';
         L.panjang = 5;
     } else if (urutan == 4){
-        L.kata[0] = "T";
-        L.kata[1] = "E";
-        L.kata[2] = "N";
-        L.kata[3] = "A";
-        L.kata[4] = "N";
-        L.kata[5] = "G";
+        L.kata[0] = 'T';
+        L.kata[1] = 'E';
+        L.kata[2] = 'N';
+        L.kata[3] = 'A';
+        L.kata[4] = 'N';
+        L.kata[5] = 'G';
         L.panjang = 6;
     } else if (urutan == 5){
-        L.kata[0] = "B";
-        L.kata[1] = "A";
-        L.kata[2] = "Y";
-        L.kata[3] = "A";
-        L.kata[4] = "R";
+        L.kata[0] = 'B';
+        L.kata[1] = 'A';
+        L.kata[2] = 'Y';
+        L.kata[3] = 'A';
+        L.kata[4] = 'R';
         L.panjang = 5;
     } else if (urutan == 6){
-        L.kata[0] = "G";
-        L.kata[1] = "A";
-        L.kata[2] = "N";
-        L.kata[3] = "T";
-        L.kata[4] = "I";
-        L.kata[5] = "A";
-        L.kata[6] = "N";
+        L.kata[0] = 'G';
+        L.kata[1] = 'A';
+        L.kata[2] = 'N';
+        L.kata[3] = 'T';
+        L.kata[4] = 'I';
+        L.kata[5] = 'A';
+        L.kata[6] = 'N';
         L.panjang = 7;
     } else if (urutan == 7){
-        L.kata[0] = "B";
-        L.kata[1] = "I";
-        L.kata[2] = "A";
-        L.kata[3] = "R";
-        L.kata[4] = "K";
-        L.kata[5] = "A";
-        L.kata[6] = "N";
+        L.kata[0] = 'B';
+        L.kata[1] = 'I';
+        L.kata[2] = 'A';
+        L.kata[3] = 'R';
+        L.kata[4] = 'K';
+        L.kata[5] = 'A';
+        L.kata[6] = 'N';
         L.panjang = 7;
     } else if (urutan == 8){
-        L.kata[0] = "B";
-        L.kata[1] = "A";
-        L.kata[2] = "K";
-        L.kata[3] = "A";
-        L.kata[4] = "T";
+        L.kata[0] = 'B';
+        L.kata[1] = 'A';
+        L.kata[2] = 'K';
+        L.kata[3] = 'A';
+        L.kata[4] = 'T';
         L.panjang = 5;
     } else if (urutan == 9){
-        L.kata[0] = "C";
-        L.kata[1] = "U";
-        L.kata[2] = "M";
-        L.kata[3] = "A";
-        L.kata[4] = "D";
-        L.kata[5] = "U";
-        L.kata[6] = "A";
-        L.kata[7] = "A";
-        L.kata[8] = "J";
-        L.kata[9] = "A";
+        L.kata[0] = 'C';
+        L.kata[1] = 'U';
+        L.kata[2] = 'M';
+        L.kata[3] = 'A';
+        L.kata[4] = 'D';
+        L.kata[5] = 'U';
+        L.kata[6] = 'A';
+        L.kata[7] = 'A';
+        L.kata[8] = 'J';
+        L.kata[9] = 'A';
         L.panjang = 10;
     } else if (urutan == 10){
-        L.kata[0] = "M";
-        L.kata[1] = "A";
-        L.kata[2] = "S";
+        L.kata[0] = 'M';
+        L.kata[1] = 'A';
+        L.kata[2] = 'S';
         L.panjang = 3;
     }
 }
@@ -255,7 +261,7 @@ void DigitBenar(List LSoal, char huruf, List Lsalah, List Lbenar){
             InsertAt(&Lbenar, LSoal, i);
             Lbenar.panjang++;
         } else {
-            InsertAt(&Lsalah, LSoal, Lsalah.panjang);
+            (Lsalah).kata[Lsalah.panjang] = huruf;
             Lsalah.panjang++;
         }
         i++;
@@ -296,19 +302,27 @@ boolean IsFinished (List Lsalah, List Lbenar, List LSoal){
     return (Lbenar.panjang == LSoal.panjang || Lsalah.panjang > KESEMPATAN);
 }
 
+
+/*
 int main() {
-    printf("Selamat datang di game hangman!!\n Semoga kamu ga menyesal bermain game ini!!");
+    printf("=======================================================================\n");
+    printf("Selamat datang di game hangman!!\nSemoga kamu ga menyesal bermain game ini!!");
+
     char input;
     int point=0;
     srand(time(0));
     List LSoal;
     List Lsalah;
     List Lbenar;
+    int urutan = (rand()%10) + 1;
+
+    variasiSoal(urutan, LSoal);
+    CreateList(&LSoal, LSoal.panjang);
+    CreateList(&Lbenar, LSoal.panjang);
+    CreateEmpty(&Lsalah);
     Lbenar.panjang = 0;
     Lsalah.panjang = 0;
-    int urutan = (rand()%10) + 1;
-    variasiSoal(urutan, LSoal);
-    CreateList1(&LSoal, LSoal.panjang);
+
     TampilanGame(Lbenar, &Lsalah, urutan);
     scanf("\nMASUKAN TEBAKAN : %c\n", &input);
     while(!((int) input >= 'A' && (int) input <= 'Z')){
@@ -335,3 +349,4 @@ int main() {
     }
     return 0;
 }
+*/
